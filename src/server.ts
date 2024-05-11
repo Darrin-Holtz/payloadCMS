f6b8d63dfbb4d8856721ed62140d15f290a46be1
darrinholtz <darrinholtz@gmail.com>
2024-05-11 19:28:59 -0400

    first commit

---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import express from "express"
+import nextBuild from 'next/dist/build'
+import path from 'path'
+import { getPayloadClient } from './get-payload'
+import { nextApp, nextHandler } from './next-utils'
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+const start = async () => {
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
+      },
+    },
+  }) 
+
+  if (process.env.NEXT_BUILD) {
+    app.listen(PORT, async () => {
+      payload.logger.info(`Next.js is now building...`)
+      // @ts-expect-error
+      await nextBuild(path.join(__dirname, '../'))
+      process.exit()
+    })
+
+    return
+  }
+
+  const nextHandler = nextApp.getRequestHandler()
+
+  app.use((req, res) => nextHandler(req, res))
+
+  nextApp.prepare().then(() => {
+    payload.logger.info('Starting Next.js...')
+
+    app.listen(PORT, async () => {
+      payload.logger.info(`Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`)
+    })
+  })
+}
+
+start()
